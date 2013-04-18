e4a01b9a55df4889df34d9ebdd60744f3cd476bf
Mike Berlin <mikeberlin@me.com>
2013-04-17 19:07:28 -0500

    Initial Commit

---
+++ MemoryCardGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MemoryCardGame
+//
+//  Created by Mike Berlin on 4/17/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MemoryCardGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MemoryCardGame
+//
+//  Created by Mike Berlin on 4/17/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
